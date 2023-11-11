ffb404aa87eda9737db1ecf9a6a25104f8461ccc
wexsilon <wexsilon2001@protonmail.com>
2023-11-12 00:02:20 +0330

    config port and host from .env

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  
-  await app.listen(3000);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+    const configService = app.get(ConfigService);
+
+    await app.listen(
+        configService.get<number>('APP_PORT'),
+        configService.get<string>('APP_HOST'),
+        () => {
+            console.log(
+                `Start on ${configService.get<number>(
+                    'APP_PORT',
+                )}:${configService.get<string>('APP_HOST')}`,
+            );
+        },
+    );
 }
 bootstrap();
