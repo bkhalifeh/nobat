365b169e33770559543a07db12d1ad32c503f253
wexsilon <wexsilon2001@protonmail.com>
2023-11-12 00:03:27 +0330

    format code

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,8 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ConfigModule.forRoot({ isGlobal: true })],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
