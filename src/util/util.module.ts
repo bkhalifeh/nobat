35b468288b704670104275c577f2720b9f53f14f
wexsilon <wexsilon2001@protonmail.com>
2023-11-12 02:30:45 +0330

    add PkId for primary key in entites

--- src/util/util.module.ts
+++ src/util/util.module.ts
@@ -3,6 +3,6 @@ import { UtilService } from './util.service';
 
 @Module({
     providers: [UtilService],
-    exports: [UtilService]
+    exports: [UtilService],
 })
 export class UtilModule {}
--- src/util/util.service.ts
+++ src/util/util.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ObjectIdColumn, PrimaryGeneratedColumn } from 'typeorm';
 
 @Injectable()
-export class UtilService {}
+export class UtilService {
+    
+    @Inject(ConfigService)
+    private static configService: ConfigService;
+    
+    public static PkId() {
+        if (UtilService.configService.get<string>('DB_TYPE') === 'mongodb') {
+            return ObjectIdColumn({ name: '_id' });
+        } else {
+            return PrimaryGeneratedColumn({
+                name: 'id',
+                type: 'int',
+                unsigned: true,
+            });
+        }
+    }
+}
