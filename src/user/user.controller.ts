458238ad1186220debf06bbb326f409c00e81b44
wexsilon <wexsilon2001@protonmail.com>
2023-11-12 13:56:14 +0330

    add work route to user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -15,5 +15,8 @@ import { UpdateUserDto } from './dto/update-user.dto';
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
-
+    @Get('work')
+    work() {
+        this.userService.work();
+    }
 }
