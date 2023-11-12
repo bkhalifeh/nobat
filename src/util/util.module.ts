a38ef43e69be0955256fce64eb8af35414f36331
wexsilon <wexsilon2001@protonmail.com>
2023-11-12 09:55:32 +0330

    import ConfigModule

--- src/util/util.module.ts
+++ src/util/util.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UtilService } from './util.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+    imports: [ConfigModule],
     providers: [UtilService],
     exports: [UtilService],
 })
