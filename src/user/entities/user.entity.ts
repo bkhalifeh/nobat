90a5ecbf363cc50f3cdb320ba1701f2763d7aa03
wexsilon <wexsilon2001@protonmail.com>
2023-11-12 14:56:42 +0330

    inheritance from CustomeEntity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,10 +1,9 @@
-import { IdColumn, IdType } from 'src/util/database';
+import { CustomeEntity } from 'src/database/custome.entity';
+import { IdColumn, IdType } from 'src/database/custome.id';
 import { Column, Entity } from 'typeorm';
 
 @Entity()
-export class User {
-    @IdColumn()
-    id: IdType;
+export class User extends CustomeEntity {
 
     @Column({ unique: true })
     phoneNumber: string;
