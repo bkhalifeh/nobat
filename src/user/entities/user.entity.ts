165d22c3088cfa2ac5254bf8511fb8e3fdb6f7c2
wexsilon <wexsilon2001@protonmail.com>
2023-11-12 11:54:00 +0330

    add Entity decorator

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,8 +1,8 @@
-import { UtilService } from "src/util/util.service";
-import { ObjectId } from "typeorm";
+import { Entity, ObjectId } from "typeorm";
 import PkId from 'src/util/database/pk.id';
+
+@Entity()
 export class User {
     @PkId()
     id: ObjectId | number;
-
 }
