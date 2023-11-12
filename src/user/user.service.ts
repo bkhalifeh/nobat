5b7163d3483119c9e2a9e727623355ca0c5f517a
wexsilon <wexsilon2001@protonmail.com>
2023-11-12 12:04:10 +0330

    implement simple findOne user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,6 +4,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { IdType } from 'src/util/database';
 
 @Injectable()
 export class UserService {
@@ -15,19 +16,9 @@ export class UserService {
         return 'This action adds a new user';
     }
 
-    findAll() {
-        return `This action returns all user`;
+    findOne(id: IdType) {
+        return this.usersRepository.findOneBy({ id });
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} user`;
-    }
 
-    update(id: number, updateUserDto: UpdateUserDto) {
-        return `This action updates a #${id} user`;
-    }
-
-    remove(id: number) {
-        return `This action removes a #${id} user`;
-    }
 }
