57268051a8d63b6acffa7168d07d854256bcd9cc
wexsilon <wexsilon2001@protonmail.com>
2023-11-12 09:56:20 +0330

    define DB_TYPE

--- src/util/util.service.ts
+++ src/util/util.service.ts
@@ -5,18 +5,10 @@ import { ObjectIdColumn, PrimaryGeneratedColumn } from 'typeorm';
 @Injectable()
 export class UtilService {
     
-    @Inject(ConfigService)
-    private static configService: ConfigService;
-    
-    public static PkId() {
-        if (UtilService.configService.get<string>('DB_TYPE') === 'mongodb') {
-            return ObjectIdColumn({ name: '_id' });
-        } else {
-            return PrimaryGeneratedColumn({
-                name: 'id',
-                type: 'int',
-                unsigned: true,
-            });
-        }
+    public static DB_TYPE: string;
+
+    constructor(configService: ConfigService) {
+        UtilService.DB_TYPE = configService.get<string>('DB_TYPE');
     }
 }
+
