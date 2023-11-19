2622bb969639d7b92369e882d5b0197750ff7c31
wexsilon <wexsilon2001@protonmail.com>
2023-11-20 01:17:16 +0330

    add function to generate random number

--- src/util/util.service.ts
+++ src/util/util.service.ts
@@ -5,4 +5,16 @@ import { ObjectIdColumn, PrimaryGeneratedColumn } from 'typeorm';
 @Injectable()
 export class UtilService {
     constructor(private readonly configService: ConfigService) {}
+
+    // static srandomNumber(min: number, max: number): number {
+    //     const minCeil = Math.ceil(min);
+    //     const maxFloor = Math.floor(max);
+    //     return Math.floor(Math.random() * (maxFloor - minCeil) + minCeil);
+    // }
+
+    randomNumber(min: number, max: number): number {
+        const minCeil = Math.ceil(min);
+        const maxFloor = Math.floor(max);
+        return Math.floor(Math.random() * (maxFloor - minCeil) + minCeil);
+    }
 }
