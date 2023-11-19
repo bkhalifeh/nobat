50d7b91eec5c071e00f4e02f0fec6022bb5c8843
wexsilon <wexsilon2001@protonmail.com>
2023-11-20 01:15:57 +0330

    implement verify-code

--- src/verify-code/verify-code.module.ts
+++ src/verify-code/verify-code.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { VerifyCodeService } from './verify-code.service';
-import { VerifyCodeController } from './verify-code.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { VerifyCode } from './entities/verify-code.entity';
+import { UtilModule } from 'src/util/util.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([VerifyCode])],
+    imports: [TypeOrmModule.forFeature([VerifyCode]), UtilModule],
     providers: [VerifyCodeService],
     exports: [VerifyCodeService],
 })
--- src/verify-code/verify-code.service.ts
+++ src/verify-code/verify-code.service.ts
@@ -1,14 +1,44 @@
-import { Injectable } from '@nestjs/common';
-import { CreateVerifyCodeDto } from './dto/create-verify-code.dto';
-import { UpdateVerifyCodeDto } from './dto/update-verify-code.dto';
+import { Injectable, NotImplementedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { VerifyCode } from './entities/verify-code.entity';
+import { UtilService } from 'src/util/util.service';
+import { IdType } from 'src/database/custome.id';
 
 @Injectable()
 export class VerifyCodeService {
     constructor(
         @InjectRepository(VerifyCode)
         private verifyCodesRepository: Repository<VerifyCode>,
+        private readonly utilService: UtilService,
     ) {}
+
+    async getVerifyCode(phoneNumber: string): Promise<string> {
+        let verifyCode = await this.verifyCodesRepository.findOneBy({
+            phoneNumber,
+        });
+        const code = this.utilService
+            .randomNumber(1, 999999)
+            .toString()
+            .padStart(6);
+        if (verifyCode) {
+            throw new NotImplementedException(
+                'VerifyCodeService - getVerifyCode',
+            );
+        } else {
+            verifyCode = await this.verifyCodesRepository.save({
+                phoneNumber,
+                code,
+            });
+        }
+        return verifyCode.code;
+    }
+
+    async findOneByPhoneNumber(phoneNumber: string) {
+        return this.verifyCodesRepository.findOneBy({ phoneNumber });
+    }
+
+    delete(id: IdType) {
+        return this.verifyCodesRepository.delete(id);
+    }
 }
