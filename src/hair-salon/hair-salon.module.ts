536a59c00143ea47bb099567d7727835014ba9c6
wexsilon <wexsilon2001@protonmail.com>
2023-11-20 08:32:29 +0330

    export HairSalonService

--- src/hair-salon/hair-salon.module.ts
+++ src/hair-salon/hair-salon.module.ts
@@ -13,5 +13,6 @@ import { UserModule } from 'src/user/user.module';
     ],
     controllers: [HairSalonController],
     providers: [HairSalonService],
+    exports: [HairSalonService],
 })
 export class HairSalonModule {}
