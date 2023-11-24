631ea2f43ea49c637d4e16e2525782ce45daeebe
wexsilon <wexsilon2001@protonmail.com>
2023-11-24 16:52:04 +0330

    change url ippanel

--- src/sms/sms.service.ts
+++ src/sms/sms.service.ts
@@ -5,16 +5,16 @@ import { Injectable } from '@nestjs/common';
 export class SmsService {
     constructor(private readonly httpService: HttpService) {}
 
-    async sendVerifyCode(phoneNumber: string, code: string) {
+    async sendVerifyCode(phoneNumber: string, vcode: string) {
         const res = await this.httpService.axiosRef.post(
-            // 'https://api2.ippanel.com/api/v1/sms/pattern/normal/send',
-            'http://rest.ippanel.com/v1/messages/patterns/send',
+            'https://api2.ippanel.com/api/v1/sms/pattern/normal/send',
+            // 'http://rest.ippanel.com/v1/messages/patterns/send',
             {
-                pattern_code: 's3zzqx5hqwfo5pj',
+                pattern_code: 'ph3nxew2kwmcurj',
                 originator: '+983000505',
                 recipient: `+98${phoneNumber}`,
                 values: {
-                    code,
+                    vcode,
                 },
             },
         );
