2d44e25ba5e3a09bb697682f0a8cb21c0e5285d4
wexsilon <wexsilon2001@protonmail.com>
2023-11-24 17:00:05 +0330

    update data in ippanel

--- src/sms/sms.service.ts
+++ src/sms/sms.service.ts
@@ -10,13 +10,13 @@ export class SmsService {
             'https://api2.ippanel.com/api/v1/sms/pattern/normal/send',
             // 'http://rest.ippanel.com/v1/messages/patterns/send',
             {
-                pattern_code: 'ph3nxew2kwmcurj',
-                originator: '+983000505',
+                code: "ph3nxew2kwmcurj",
+                sender: "+983000505",
                 recipient: `+98${phoneNumber}`,
-                values: {
-                    vcode,
-                },
-            },
+                variable: {
+                    vcode
+                }
+            }
         );
         return res.data;
     }
